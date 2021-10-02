310df3d37527c2209643fffab5117baf1fb380fa
Lenovo <shavarsh101097@mail.ru>
2021-10-02 19:31:51 +0400

    nest test

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,7 @@
 import {Body, Controller, Post} from '@nestjs/common';
-import {ApiTags} from "@nestjs/swagger";
 import {CreateUserDto} from "../users/dto/create-user.dto";
 import {AuthService} from "./auth.service";
 
-@ApiTags('Авторизация')
 @Controller('auth')
 export class AuthController {
 
